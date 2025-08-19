febd1f16907249d0029c34bdd1f7a426d885e60d
Moises Gomez <moisesgomezr9@gmail.com>
2025-08-18 19:21:36 -0700

    add(client) add initial React setup with main.tsx as entry point. Create main.tsx with create root mounting the component being exported from App.tsx, imported global styles from index.css and create the base App.tsx component. We are using utility styling for our components

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App(){
+    return (
+        <main className="p-8 font-sans">
+            <h1 className="text-3x1 font-bold">Mastermind Game</h1>
+        </main>
+    )
+}
