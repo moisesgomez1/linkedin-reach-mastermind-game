065f902899b1b9440529d3fdb1d7169712693967
Moises Gomez <moisesgomezr9@gmail.com>
2025-08-19 14:11:40 -0700

    add: made the function async since we will be using a try catch block in which we will make the fetch request. when making a call in postman to the random api the response was a plain text response so using the text method to read and parse the plain text response.

--- server/services/randomService.ts
+++ server/services/randomService.ts
@@ -1,3 +1,34 @@
+require('dotenv').config();
 //creating the service function that will make a request to the api to generate a random number
 
-export function fetchRandomNumbers(): any {}
+export async function fetchRandomNumbers(): Promise<string> {
+  const baseUrl = process.env.RANDOM_ORG_BASE_URL;
+
+  //fixed query params for now
+
+  const num = 4;
+  const min = 0;
+  const max = 7;
+
+  const url = `${baseUrl}?num=${num}&min=${min}&max=${max}&col=1&base=10&format=plain&rnd=new`;
+
+  try {
+    const response = await fetch(url);
+
+    // If the response is not OK, throw an error
+    if (!response.ok) {
+      throw new Error(`Random.org error: ${response.statusText}`);
+    }
+
+    // Read and parse the plain text response
+    const numbers = await response.text();
+
+    console.log ("This is the result of the call to random.org", numbers)
+
+    return numbers;
+  } catch (error) {
+    console.error('Random.org failed');
+
+    return 'error';
+  }
+}
