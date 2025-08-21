a34cec342ac2cdec9f833c2dfc69ffd5d51d5353
Moises Gomez <moisesgomezr9@gmail.com>
2025-08-21 12:57:21 -0700

    add: created Mastermind game page and introduced React Router to be able to navigate between pages on events

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Mastermind from "./pages/MasterMind";
 
 export default function App() {
-    return (
-        <main className="p-8 font-sans">
-            <Home/>
-        </main>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Landing page */}
+        <Route path="/" element={<Home />} />
+        {/* Game page */}
+        <Route path="/mastermind" element={<Mastermind />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
